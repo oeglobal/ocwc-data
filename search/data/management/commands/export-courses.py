b746eb8ed15237cc4a0547ccc4a18a3dbc8dc8a3
Jure Cuhalev <gandalfar@gmail.com>
2014-01-02 04:12:26 -0800

    new export-courses command, tests

---
+++ search/data/management/commands/export-courses.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import xlwt
+from optparse import make_option
+from collections import OrderedDict
+
+from django.core.management.base import BaseCommand
+
+from data.models import Provider
+
+class Command(BaseCommand):
+    help = "Exports list of OCW courses to XLS and TSV"
+    args = "--xls <file> --tsv <file>"
+
+    option_list = BaseCommand.option_list + (
+        make_option("--xls", action="store", dest="xls_filename", help="XLS filename"),
+    )
+
+    def handle(self, *args, **options):
+        if options.get("xls_filename"):
+            self.export_xls(options.get("xls_filename"))
+
+    def export_xls(self, xls_filename):
+        wbk = xlwt.Workbook()
+        sheet = wbk.add_sheet('Courses')
+
+        excel_date_fmt = 'D.M.Y'
+        style = xlwt.XFStyle()
+        style.num_format_str = excel_date_fmt
+
+        first_row = OrderedDict([
+            ('OCW Link', 18000),    # 0
+            ('URL Hash', 8000),     # 1
+            ('Link', 20000),         # 2
+            ('Provider', 8000),     # 3
+            ('Language', 5000),     # 4
+            ('Tags', 8000),         # 5
+            ('Author', 8000),       # 6
+            ('Title', 10000),        # 7
+            ('Description', 10000),  # 8
+            ('Published', 5000),    # 9
+            ('Indexed', 5000),      # 10
+            ('Modified', 5000)      # 11
+        ])
+
+        r = c = 0
+
+        for item in first_row:
+            sheet.write(r, c, item)
+            sheet.col(c).width = first_row[item]
+            c += 1
+        r += 1
+
+        link = 'http://www.ocwconsortium.org/courses/view/%s/'
+
+        for provider in Provider.objects.all():
+            for source in provider.source_set.all():
+                for course in source.course_set.all():
+
+                    sheet.write(r, 0, link % course.linkhash)
+                    sheet.write(r, 1, course.linkhash )
+                    sheet.write(r, 2, course.linkurl )
+                    sheet.write(r, 3, provider.name )
+                    sheet.write(r, 4, course.language )
+                    sheet.write(r, 5, course.tags )
+                    sheet.write(r, 6, course.author )
+                    sheet.write(r, 7, course.title )
+                    sheet.write(r, 8, course.description )
+                    sheet.write(r, 9, course.date_published, style)
+                    sheet.write(r, 10, course.date_indexed,   style)
+                    sheet.write(r, 11, course.date_modified,  style)
+
+                    r += 1
+
+        wbk.save(xls_filename)
+
+        self.stdout.write("Wrote %s courses to %s" % (r, xls_filename))
--- search/data/tests.py
+++ search/data/tests.py
@@ -1,16 +1,16 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.test import TransactionTestCase as TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class testExportCourses(TestCase):
+    fixtures = ['test-fixtures.json']
+
+    def testXlsExport(self):
+        from django.core import management
+        import os
+        with open('/tmp/command_output', 'w') as f:
+            management.call_command('export-courses', xls_filename='/tmp/test-export.xls', stdout=f)
+
+        with open('/tmp/command_output', 'r') as f:
+            self.assertIn('Wrote 4 courses to /tmp/test-export.xls', f.read())
+
+        os.remove('/tmp/command_output')
