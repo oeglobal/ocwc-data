fc3f17dd16a5d39f101969cbc62c793d9845ff4e
Jure Cuhalev <gandalfar@gmail.com>
2014-02-13 20:02:50 +0100

    course detail listing

--- search/web/urls_dashboard.py
+++ search/web/urls_dashboard.py
@@ -4,4 +4,5 @@ from .views import dashboard
 
 urlpatterns = patterns('',
 	url(r'^source/(?P<pk>\d+)/course/list/$', dashboard.SourceCourseListView.as_view(), name='source-course-list'),
+	url(r'^course/(?P<linkhash>\w+)/$', dashboard.CourseDetailView.as_view(lookup_field='linkhash'), name='course-view'),
 )
--- search/web/views/dashboard.py
+++ search/web/views/dashboard.py
@@ -4,15 +4,19 @@ from vanilla import ListView, DetailView, TemplateView, RedirectView, UpdateView
 from data.models import Course, Provider, Source, Category
 
 class SourceCourseListView(ListView):
-	model = Course
-	template_name = 'web/source_course_list.html'
+    model = Course
+    template_name = 'web/source_course_list.html'
 
-	def get_queryset(self):
-		pk = self.kwargs.get('pk')
-		return self.model.objects.filter(source=pk)
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        return self.model.objects.filter(source=pk)
 
-	def get_context_data(self, **kwargs):
-		context = super(SourceCourseListView, self).get_context_data(**kwargs)
-		context['source'] = Source.objects.get(pk=self.kwargs.get('pk'))
-		
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(SourceCourseListView, self).get_context_data(**kwargs)
+        context['source'] = Source.objects.get(pk=self.kwargs.get('pk'))
+        
+        return context
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'web/course_detail.html'
