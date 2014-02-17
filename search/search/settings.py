a503e86a0be1df7962a4081fc70b7029a426d07d
Jure Cuhalev <gandalfar@gmail.com>
2014-02-18 00:18:50 +0100

    use relative paths

--- search/search/settings.py
+++ search/search/settings.py
@@ -27,7 +27,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = next_to_root('/media/')
+MEDIA_ROOT = next_to_root('media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -38,7 +38,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = next_to_root('/static/')
+STATIC_ROOT = next_to_root('static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
